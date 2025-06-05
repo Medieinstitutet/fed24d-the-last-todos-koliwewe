421e56470b78e2cd1155d41fa2b4cc190a03a23f
Sokol Hatija <hatijasokol@gmail.com>
2025-06-06 01:51:54 +0200

    some small changes and fix

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,21 +1,34 @@
 import { type Todo } from "../models/Todo";
 
 interface Props {
-    todos: Todo[];
-    toggleTodo: (id: number) => void;
-    deleteTodo: (id: number) => void;
+  todos: Todo[];
+  toggleTodo: (id: number) => void;
+  deleteTodo: (id: number) => void;
 }
 
 const TodoList = ({ todos, toggleTodo, deleteTodo }: Props) => {
   return (
     <ul className="todo-list">
       {todos.map((todo) => (
-        <li key={todo.id} className={todo.done ? 'done' : ''}>
-          <div onClick={() => toggleTodo(todo.id)}>
+        <li key={todo.id} className={`todo-card ${todo.done ? 'done' : ''}`}>
+          <div className="todo-content">
             <h3>{todo.title}</h3>
             <p>{todo.description}</p>
           </div>
-          <button className="delete-button" onClick={() => deleteTodo(todo.id)}>🗑 Ta bort</button>
+          <div className="button-group">
+            <button
+              className="toggle-button"
+              onClick={() => toggleTodo(todo.id)}
+            >
+              ✅ {todo.done ? "Ångra" : "Klar"}
+            </button>
+            <button
+              className="delete-button"
+              onClick={() => deleteTodo(todo.id)}
+            >
+              🗑 Ta bort
+            </button>
+          </div>
         </li>
       ))}
     </ul>
